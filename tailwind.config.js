4377d95f906f752dbc4499826b5378f4b2d0f493
KevStatic <kev.static11@gmail.com>
2023-07-18 09:50:53 +0400

    Fixed output.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [HTML/AdventuraHomePage.html],
-  content: [assets/JavaScript/WebCraftJS.js],
-  //if it doesnt work, change the content to: "./HTML/Adventura.html" {add more content's as HTML files increase}
+  content: ["./HTML/*.{js,ts,html}"],
   theme: {
     extend: {},
   },
