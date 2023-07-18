1665200c8af4b3f688af9efb302da26cfabc6629
KevStatic <kev.static11@gmail.com>
2023-07-18 04:00:25 +0400

    Added TailwindCSS config
    Node modules added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [HTML/AdventuraHomePage.html],
+  content: [assets/JavaScript/WebCraftJS.js],
+  //if it doesnt work, change the content to: "./HTML/Adventura.html" {add more content's as HTML files increase}
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
