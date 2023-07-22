ca0171490dba63924de47ad269ac30301ef61ab4
LamyaGandhi <139221262+LamyaGandhi@users.noreply.github.com>
2023-07-22 22:00:38 +0400

    Add files via upload

--- assets/JavaScript/contact.js
+++ assets/JavaScript/contact.js
@@ -1,52 +1,70 @@
-const carousel = document.querySelector('.carousel');
-const carouselItems = Array.from(carousel.children);
-const carouselContainer = document.querySelector('.carousel-container');
-const prevBtn = document.querySelector('.carousel-prev');
-const nextBtn = document.querySelector('.carousel-next');
-const boxWidth = carouselContainer.offsetWidth;
-const numVisibleBoxes = Math.floor(boxWidth / carouselItems[0].offsetWidth);
-const numTotalBoxes = carouselItems.length;
-let currentIndex = 0;
+// Add JavaScript for the carousel navigation
+let slideIndex = 0;
 
-prevBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex - numVisibleBoxes + numTotalBoxes) % numTotalBoxes;
-  showSlide();
-});
-
-nextBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex + numVisibleBoxes) % numTotalBoxes;
-  showSlide();
-});
-
-function showSlide() {
-  const distance = -currentIndex * carouselItems[0].offsetWidth;
-  carousel.style.transform = `translateX(${distance}px)`;
+function showSlide(n) {
+  const slides = document.getElementsByClassName("carousel-item");
+  if (n >= slides.length) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "flex";
 }
 
-window.addEventListener('resize', () => {
-  const newBoxWidth = carouselContainer.offsetWidth;
-  const newNumVisibleBoxes = Math.floor(newBoxWidth / carouselItems[0].offsetWidth);
+function prevSlide() {
+  showSlide(slideIndex - 1);
+}
 
-  if (newNumVisibleBoxes !== numVisibleBoxes) {
-    currentIndex = Math.floor(currentIndex / numVisibleBoxes) * newNumVisibleBoxes;
-    numVisibleBoxes = newNumVisibleBoxes;
-    showSlide();
+function nextSlide() {
+  showSlide(slideIndex + 1);
+}
+
+// Set an interval to automatically move to the next slide every 2 seconds
+setInterval(nextSlide, 2000);
+
+// Form validation
+function validateForm() {
+  // Add your form validation logic here
+  return true; // Return false to prevent form submission
+}
+
+// Show the initial slide
+showSlide(slideIndex);
+
+
+// Form validation
+function validateForm() {
+  const fname = document.getElementById("fname").value;
+  const lname = document.getElementById("lname").value;
+  const phone = document.getElementById("phone").value;
+  const email = document.getElementById("email").value;
+  const comment = document.getElementById("comment").value;
+
+  // Check if any field is empty
+  if (fname.trim() === "" || lname.trim() === "" || phone.trim() === "" || email.trim() === "" || comment.trim() === "") {
+    alert("All fields are required.");
+    return false;
   }
-});
 
-// Optional: Automatically start the carousel
-let intervalId = setInterval(() => {
-  currentIndex = (currentIndex + numVisibleBoxes) % numTotalBoxes;
-  showSlide();
-}, 5000);
+  // Validate phone number (Example: 1-123-456-7890)
+  const phoneRegex = /^\d{1,3}-\d{3}-\d{3}-\d{4}$/;
+  if (!phone.match(phoneRegex)) {
+    alert("Please enter a valid phone number in the format 1-123-456-7890.");
+    return false;
+  }
 
-carouselContainer.addEventListener('mouseover', () => {
-  clearInterval(intervalId);
-});
+  // Validate email address
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email.match(emailRegex)) {
+    alert("Please enter a valid email address.");
+    return false;
+  }
 
-carouselContainer.addEventListener('mouseout', () => {
-  intervalId = setInterval(() => {
-    currentIndex = (currentIndex + numVisibleBoxes) % numTotalBoxes;
-    showSlide();
-  }, 5000);
-});
+  return true;
+}
+
+
+
